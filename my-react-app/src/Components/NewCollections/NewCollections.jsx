99456f1528518535cefec0cee96500f49a2a5fe1
sudhir-he <sudhir868198@gmail.com>
2024-01-23 10:09:12 +0530

    adding assest and new component

---
+++ my-react-app/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NewCollections.css' ;
+const NewCollections = () => {
+    return(<div className="new-collections"> 
+         <h1>NEW COLLECTIONS</h1>
+         <hr/>
+    </div>);
+}
+
+export default NewCollections;
