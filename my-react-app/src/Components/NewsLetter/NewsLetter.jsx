38c4bf83925b57c112a925ae18b0cfc6b5b1f575
Sakthiadhavan <sakthiadhavan452@gmail.com>
2024-01-25 09:51:34 +0530

    shop page completed

---
+++ my-react-app/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NewsLetter.css'
+const NewsLetter = () => {
+  return (
+    <div>
+      <div className="newsletter">
+        <h1>Get Exclusive Offers On your Email</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
+      
+      <div>
+        <input type="email" placeholder='your Email here'/>
+        <button>Subscribe</button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default NewsLetter
--- my-react-app/src/Pages/Shop.jsx
+++ my-react-app/src/Pages/Shop.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offer from '../Components/Offers/Offer'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Footer from '../Components/Footer/Footer'
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
       <Offer/>
+      <NewCollections/>
+      <NewsLetter/>
+      <Footer/>
     </div>
   )
 }
