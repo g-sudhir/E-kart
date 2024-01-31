d316e53580a790af340a8c56976c3ef686c874d0
sudhir-he <sudhir868198@gmail.com>
2024-01-31 09:11:47 +0530

    small chnages

--- my-react-app/src/Components/ProductDisplay/ProductDisplay.jsx
+++ my-react-app/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -28,7 +28,7 @@ const ProductDisplay = (props) => {
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
-                <img src="" alt={star_dull_icon} />
+                <img src={star_dull_icon} alt="" />
                 <p>(122)</p>
                </div>
                <div className="productdisplay-right-prices">
@@ -38,9 +38,9 @@ const ProductDisplay = (props) => {
                <div className="productdisplay-right-description">
                    A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
                </div>
-               <div className="produtcdisplay-right-size">
+               <div className="productdisplay-right-size">
                 <h1>Select Size</h1>
-                <div className="productdisplay-right-size">
+                <div className="productdisplay-right-sizes">
                     <div>S</div>
                     <div>M</div>
                     <div>L</div>
